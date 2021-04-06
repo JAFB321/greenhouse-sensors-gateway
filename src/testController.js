bd0c04e9c4795a6f4455560a2085077ca9dcc883
JAFB321 <jafb321@gmail.com>
2021-04-05 18:24:41 -0700

    fix: listener call params

--- src/testController.js
+++ src/testController.js
@@ -2,17 +2,17 @@ class TestController {
 	onSensorValue(listener = (sensorID, value) => {}) {
 		setInterval(() => {
 			const random = Math.floor(Math.random() * 5);
-			listener(30 + random, 'TEMP1');
+			listener('TEMP1', 30 + random);
 		}, 300);
 
 		setInterval(() => {
 			const random = Math.floor(Math.random() * 10);
-			listener(34 + random, 'TEMP2');
+			listener('TEMP2', 34 + random);
 		}, 200);
 
 		setInterval(() => {
 			const random = Math.floor(Math.random() * 5);
-			listener(1 + random, 'HUM1');
+			listener('HUM1', 1 + random);
 		}, 250);
 	}
 }
