c9fbe6438554e3b6d4e9d706564384e3d23b4436
JAFB321 <jafb321@gmail.com>
2021-04-22 16:32:55 -0700

    feat: add rabbitmq controller class

---
+++ src/controllers/rabbitmqController.js
@@ -0,0 +1,44 @@
+const amqp = require('amqplib');
+
+class rabbitmqController {
+	constructor({ HOST, Queue }) {
+		this.HOST = HOST;
+		this.Queue = Queue;
+	}
+
+	async init(onSensorValue) {
+		// this.onSensorValue = onSensorValue;
+
+		const connection = await amqp.connect('amqp://localhost');
+		const channel = await connection.createChannel();
+
+		// channel.bindQueue('')
+		await channel.assertQueue(this.Queue, {
+			durable: true,
+		});
+
+		channel.consume(this.Queue, async (msg) => {
+			try {
+				const sensor = JSON.parse(msg.content.toString());
+				console.log(sensor, '\n');
+
+				const { sensorID, value } = sensor;
+				await onSensorValue(sensorID, value);
+
+				channel.ack(msg);
+			} catch (error) {
+				console.log('error on send:');
+				console.log(error);
+			}
+		});
+
+		// setTimeout(function () {
+		// 	channel.close();
+		// 	conn.close();
+		// }, 500);
+	}
+}
+
+module.exports = {
+	rabbitmqController,
+};
